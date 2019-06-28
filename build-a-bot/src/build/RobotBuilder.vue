023a46781c4d52fb9af7fd7ec6c71c20c626161d
Nicholas Hafner <nickhafner@outlook.com>
2019-06-28 18:33:24 -0500

    Added computed to simply template and fixed a logical error

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -2,31 +2,31 @@
      <div>
     <div class="top-row">
       <div class="top part">
-        <img :src="availableParts.heads[selectedHeadIndex].src" title="head"/>
+        <img :src="selectedRobot.head.src" title="head"/>
         <button @click="selectPrevHead()" class="prev-selector">&#9668;</button>
         <button @click="selectNextHead()" class="next-selector">&#9658;</button>
       </div>
     </div>
     <div class="middle-row">
       <div class="left part">
-        <img :src="availableParts.arms[selectedArmLeftIndex].src" title="left arm"/>
+        <img :src="selectedRobot.leftArm.src" title="left arm"/>
         <button @click="selectPrevLeftArm()" class="prev-selector">&#9650;</button>
         <button @click="selectNextLeftArm()" class="next-selector">&#9660;</button>
       </div>
       <div class="center part">
-        <img :src="availableParts.torsos[selectedTorsoIndex].src" title="left arm"/>
+        <img :src="selectedRobot.torso.src" title="left arm"/>
         <button @click="selectPrevTorso()" class="prev-selector">&#9668;</button>
         <button @click="selectNextTorso()" class="next-selector">&#9658;</button>
       </div>
       <div class="right part">
-        <img :src="availableParts.arms[selectedArmRightIndex].src" title="right arm"/>
+        <img :src="selectedRobot.rightArm.src" title="right arm"/>
         <button @click="selectPrevRightArm()" class="prev-selector">&#9650;</button>
         <button @click="selectNextRightArm()" class="next-selector">&#9660;</button>
       </div>
     </div>
     <div class="bottom-row">
       <div class="bottom part">
-        <img :src="availableParts.bases[selectedBaseIndex].src" title="left arm"/>
+        <img :src="selectedRobot.base.src" title="left arm"/>
         <button @click="selectPrevBase()" class="prev-selector">&#9668;</button>
         <button @click="selectNextBase()" class="next-selector">&#9658;</button>
       </div>
@@ -42,8 +42,8 @@
    return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
  } 
  function getNextValidIndex(index, length) {
-   const incrementedIndex = index - 1;
-   return incrementedIndex > 0 ? length - 1 : incrementedIndex;
+   const incrementedIndex = index + 1;
+   return incrementedIndex > length - 1 ? 0 : incrementedIndex;
  }
  export default {
      name: "Robot",
@@ -56,6 +56,17 @@
          selectedArmRightIndex: 1,
          selectedBaseIndex: 1,
        };
+     },
+     computed: {
+       selectedRobot() {
+         return {
+           head: availableParts.heads[this.selectedHeadIndex],
+           leftArm: availableParts.arms[this.selectedArmLeftIndex],
+           torso: availableParts.torsos[this.selectedTorsoIndex],
+           rightArm: availableParts.arms[this.selectedArmRightIndex],
+           base: availableParts.bases[this.selectedBaseIndex],           
+         };
+       }
      },
      methods: {
        selectNextHead() {
